6149b0957deaeb79c478af1b66ca717a791e6f27
karalkal <karalkal@yahoo.co.uk>
2022-01-26 07:04:21 +0000

    Added custom manager to models.py

--- task4_blog_django_3_by_example/models.py
+++ task4_blog_django_3_by_example/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset() \
+            .filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -22,10 +29,17 @@ class Post(models.Model):
                               choices=STATUS_CHOICES,
                               default='draft')
 
+    """
+    custom manager to retrieve all posts with the published status.
+    Post.objects.my_manager()
+    or
+    Post.my_manager.all()
+    """
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
-class Meta:
-    ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
