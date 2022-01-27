5d41d8b9ee12d3c7d9f21f9b21d586f9c709120d
karalkal <karalkal@yahoo.co.uk>
2022-01-27 08:01:13 +0000

    Added urls with Django template tags, linked to css, fixed STATIC_URL in settings.py

--- django_demos/settings.py
+++ django_demos/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # FFS, I swear originally there wasn't a slash before
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- task4_blog_django_3_by_example/views.py
+++ task4_blog_django_3_by_example/views.py
@@ -11,7 +11,7 @@ from .models import Post
 def post_list(request):
     posts = Post.published.all()
     return render(request,
-                  'blog/post/list.html',
+                  'blog1/post/list.html',
                   {'posts': posts})  # 'posts' key is linked to queryset of all Post objects
 
 
@@ -23,5 +23,5 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request,
-                  'blog/post/detail.html',
+                  'blog1/post/detail.html',
                   {'post': post})
