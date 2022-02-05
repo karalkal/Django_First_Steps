c8993acac359ab97af6f006217f7c1e128531933
karalkal <karalkal@yahoo.co.uk>
2022-02-05 23:54:51 +0000

    regitestered models of 2 and 3 tasks with decorator (@admin.register), added list display

--- task2_SU_lec1/admin.py
+++ task2_SU_lec1/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from task2_SU_lec1.models import Task
 
-admin.site.register(Task)
 
+# admin.site.register(Task)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_title', 'task_text')
--- task3_print_invoices/admin.py
+++ task3_print_invoices/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from task3_print_invoices.models import Order
 
-admin.site.register(Order)
+
+# admin.site.register(Order)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('person_name', 'product', 'company', 'ship_date', 'ordered_warranty')
